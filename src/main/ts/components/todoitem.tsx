dff5812b136d61fbf968ace0523c6594cd97fad3
Hui Cao <nonocast@gmail.com>
2016-07-27 02:01:57 +0800

    v0.2.9 bootcdn.cn对react和jquery加速

--- src/main/ts/components/todoitem.tsx
+++ src/main/ts/components/todoitem.tsx
@@ -22,30 +22,6 @@ export class TodoItem extends React.Component<ITodoItemProps, ITodoItemState> {
 					</label>
 				</div>
 			</li>
-
-			//<li className={React.addons.classSet({
-      //   completed: this.props.todo.completed,
-      //   editing: this.props.editing
-      // })}>
-			//	<div className="view">
-			//		<input
-			//			className="toggle"
-			//			type="checkbox"
-			//			checked={this.props.todo.completed}
-			//			onChange={this.props.onToggle} />
-			//		<label onDoubleClick={ e => this.handleEdit() }>
-			//			{this.props.todo.title}
-			//		</label>
-			//		<button className="destroy" onClick={this.props.onDestroy} />
-			//	</div>
-			//	<input
-			//		ref="editField"
-			//		className="edit"
-			//		value={this.state.editText}
-			//		onBlur={ e => this.handleSubmit(e) }
-			//		onChange={ e => this.handleChange(e) }
-			//		onKeyDown={ e => this.handleKeyDown(e) } />
-			//</li>
 		);
 	}
 }
